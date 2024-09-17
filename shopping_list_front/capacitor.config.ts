3bb2e26225ebf74883e15e1449b588f45b6a6bbb
Camilo <camilo.coelho.gomes@gmail.com>
2024-09-17 19:55:58 -0300

    starting the front end

---
+++ shopping_list_front/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'shopping_list_front',
+  webDir: 'dist'
+};
+
+export default config;
