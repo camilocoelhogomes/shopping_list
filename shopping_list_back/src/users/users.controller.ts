72bdb09af9591dbd96229f7dd67de9b92691ce03
Camilo <camilo.coelho.gomes@gmail.com>
2024-09-21 08:25:47 -0300

    ajuste no swagger matches

--- shopping_list_back/src/users/users.controller.ts
+++ shopping_list_back/src/users/users.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 201, description: 'User created' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
