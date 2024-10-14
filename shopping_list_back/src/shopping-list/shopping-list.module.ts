a7f40750ddb73fc951377e8b05f202ecdd547585
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2024-10-14 20:28:32 -0300

    recomeçando o back end do zero

--- shopping_list_back/src/shopping-list/shopping-list.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ShoppingListService } from './shopping-list.service';
-import { ShoppingListController } from './shopping-list.controller';
-
-@Module({
-  controllers: [ShoppingListController],
-  providers: [ShoppingListService],
-})
-export class ShoppingListModule {}
