1c19080f5e9bc03b5ec52ab964d2dc8bb2909d3a
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2024-10-18 22:48:22 -0300

    ajuste no force sign in para remover os valores em caso de logout

--- shopping_list_front/src/onboarding/OnboardingUser.tsx
+++ shopping_list_front/src/onboarding/OnboardingUser.tsx
@@ -3,7 +3,7 @@ import { useAppSelector } from "../store/hook";
 import { Header } from "../components/Header";
 
 export const OnboardingUser: React.FC = () => {
-  const user = useAppSelector(s => s.auth);
+
   return (
     <IonPage>
       <Header pageHeader="Bem vindo!" />
