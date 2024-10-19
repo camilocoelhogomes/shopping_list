b944a02c6b9414f7abbf4ad6fb37c17cad90a5be
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2024-10-19 10:40:47 -0300

    ajuste na primeira tela de login (#9)

--- shopping_list_front/src/auth/ForceSignIn.tsx
+++ shopping_list_front/src/auth/ForceSignIn.tsx
@@ -3,28 +3,18 @@ import { useContext, useEffect } from "react";
 import { FirebaseContext } from "../firebase/FirebaseContext";
 import { useAppDispatch } from "../store/hook";
 import { setUser } from "../store/store_slice/userSlice";
+import { useAuth } from "./useAuth";
 
 export const ForceSignIn = ({ children }: { children: JSX.Element }) => {
   const history = useHistory();
-  const firebase = useContext(FirebaseContext);
+  const { getToken } = useAuth();
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    validateToken();
-  }, []);
-
-  const validateToken = async () => {
-    try {
-      if (!firebase?.user) return resignUser();
-      await firebase.user.getIdToken();
-    } catch (error) {
-      resignUser();
-    }
-  };
-
-  const resignUser = () => {
+  const token = getToken();
+  if (!token) {
     dispatch(setUser())
     history.push("/auth");
-  }
+    return <></>
+  };
 
   return children;
 
--- shopping_list_front/src/onboarding/OnboardingUser.tsx
+++ shopping_list_front/src/onboarding/OnboardingUser.tsx
@@ -1,14 +1,62 @@
-import { IonAvatar, IonPage } from "@ionic/react"
-import { useAppSelector } from "../store/hook";
+import { IonButton, IonContent, IonInput, IonItem, IonLabel, IonPage, IonRadio, IonRadioGroup, IonText } from "@ionic/react"
 import { Header } from "../components/Header";
+import { useContext, useEffect } from "react";
+import { FirebaseContext } from "../firebase/FirebaseContext";
+import { useAppDispatch, useAppSelector } from "../store/hook";
+import { setUser } from "../store/store_slice/userSlice";
+
+interface User {
+  name: string;
+  authProviderId: string;
+  authProvider: string;
+
+}
 
 export const OnboardingUser: React.FC = () => {
 
-  
+  const userAuth = useContext(FirebaseContext)!.user!;
+  const user = useAppSelector(s => s.user)!;
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(setUser({
+      ...user,
+      preferredName: user.displayName?.split(' ')[0] ?? undefined,
+    }))
+  }, [])
 
   return (
     <IonPage>
-      <Header pageHeader="Bem vindo!" />
+      <Header pageHeader="" />
+      <IonContent>
+        <IonItem lines="none">
+          <IonText color="dark">
+            <h1>
+              Bem Vindo {userAuth.displayName}!
+            </h1>
+          </IonText>
+        </IonItem>
+        <IonRadioGroup
+          value={user.title}
+          onIonChange={e => dispatch(setUser({ ...user, title: e.target.value }))}>
+          <IonItem lines="none">
+            <IonRadio slot="start" value="Sr."> Sr. </IonRadio>
+            <IonRadio slot="start" value="Sra." > Sra. </IonRadio>
+            <IonItem>
+              <IonInput
+                value={user.preferredName}
+                onIonChange={e => dispatch(setUser({ ...user, preferredName: e.target.value?.toString() }))}
+                placeholder="Como gostaria de ser chamado?"
+                label="Confirme seu primeiro nome"
+                labelPlacement="stacked"
+              />
+            </IonItem>
+
+          </IonItem>
+        </IonRadioGroup>
+
+
+      </IonContent>
     </IonPage>
   )
 }
