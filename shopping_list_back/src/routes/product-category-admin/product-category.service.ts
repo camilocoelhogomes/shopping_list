2d6299ae6498564e1c6cb460baae548431afddfd
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2024-11-03 11:26:52 -0300

    adição da validação de nome da categoria quando for criar uma nova

--- shopping_list_back/src/routes/product-category-admin/product-category.service.ts
+++ shopping_list_back/src/routes/product-category-admin/product-category.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { ConflictException, Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { ProductCategory } from '../../data-base/entity/product-category.entity';
 import { DatabaseDiTokens } from '../../data-base/DatabaseDiTokens';
 import { Repository } from 'typeorm';
@@ -14,6 +14,10 @@ export class ProductCategoryService {
   ) { }
 
   async create(createProductCategoryDto: Partial<ProductCategory>) {
+    const productCategory = await this.productCategoryRepository.findOne({ where: { categoryName: createProductCategoryDto.categoryName, merchantId: createProductCategoryDto.merchantId } });
+    if (productCategory) {
+      throw new ConflictException(`Product category with name ${createProductCategoryDto.categoryName} already exists`);
+    }
     const result = await this.productCategoryRepository.save(this.productCategoryRepository.create(createProductCategoryDto));
     return result;
   }
