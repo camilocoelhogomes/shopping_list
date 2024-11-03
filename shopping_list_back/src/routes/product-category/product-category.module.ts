cb6bf544fea78d34012efe43201639385ae8c5db
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2024-11-03 10:53:22 -0300

    ajuste no lint e configuração dos decoratord no product category

--- shopping_list_back/src/routes/product-category/product-category.module.ts
+++ shopping_list_back/src/routes/product-category/product-category.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ProductCategoryService } from './product-category.service';
 import { ProductCategoryController } from './product-category.controller';
+import { MiddlewrersModule } from '../../middlewrers/middlewrers.module';
+import { DataBaseModule } from '../../data-base/data-base.module';
+import { FirebaseModule } from '../../firebase/firebase.module';
+import { AuthMiddleware } from '../../middlewrers/auth/auth.middleres';
 
 @Module({
   controllers: [ProductCategoryController],
   providers: [ProductCategoryService],
+  imports: [MiddlewrersModule, DataBaseModule, FirebaseModule],
 })
-export class ProductCategoryModule {}
+export class ProductCategoryModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(ProductCategoryController);
+  }
+}
